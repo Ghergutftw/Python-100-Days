f397615a9548f80aa4d79ac8da267602f1592b9c
madal <madalinghergut@gmail.com>
2024-08-09 14:07:33 +0300

    Error handling, JSON , More Tkinter

--- 30/main.py
+++ 30/main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+# FileNotFound
+# with open("a_file.txt") as f:
+#     f.read()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+# IndexError
+# fruit_list = ["Apple", "Banana", "Pear"]
+# fruit = fruit_list[3]
+
+# TypeError
+# text = "abc"
+# print(text + 5)
+try:
+    file = open("a_file.txt")
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except TypeError as error_message:
+    print(f"Error: {error_message}")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
+
+raise TypeError("This is an error message.")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
